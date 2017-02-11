f30ad172aa2ef727a4df0fbd17f8d848427e260b
Jared Pearson <jaredapearson@gmail.com>
2017-02-10 21:39:16 -0800

    Added a 404 page

---
+++ web/public/error404.php
@@ -0,0 +1,14 @@
+<?hh
+require_once __DIR__.'/../vendor/autoload.php';
+echo '<!DOCTYPE html>';
+echo 
+<html>
+<head>
+<title>Movies - Page Not Found</title>
+<link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet" />
+<link href="/css/main.css" rel="stylesheet" />
+</head>
+<body>
+<h1>Page Not Found</h1>
+</body>
+</html>;
